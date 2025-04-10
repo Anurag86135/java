56954aec8e7c3874122e6efd7de98260c70c4f93
Anurag86135 <anuragmishra11904@gmail.com>
2025-04-10 17:34:47 +0530

    addition, substraction multiplication and division

---
+++ github1.java
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+//Addition,Substraction,Multiplication,Division of two numbers.
+import java.util.Scanner;
+class github1{
+    public static void main(String[] args) {
+        int a,b;
+        System.out.println("enter a and b number");
+        Scanner sc =new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Addition :"+(a+b));
+        System.out.println("Substraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        sc.close();
+
+        //System.out.println("hello");
+        
+    }
+=======
+//Addition,Substraction,Multiplication,Division of two numbers.
+import java.util.Scanner;
+class github1{
+    public static void main(String[] args) {
+        int a,b;
+        System.out.println("enter a and b number");
+        Scanner sc =new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Addition :"+(a+b));
+        System.out.println("Substraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        sc.close();
+
+        //System.out.println("hello");
+        
+    }
+>>>>>>> 87abcdd2b8ff732b5e5afe42e5520506218e7516
+}
