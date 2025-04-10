6fd5f189d41153b2b7f933977b23780bf32e2f58
Anurag86135 <anuragmishra11904@gmail.com>
2025-04-10 17:07:40 +0530

    Vowel or Consonant

---
+++ anurag11.java
@@ -0,0 +1,11 @@
+public class anurag11 {
+    public static void main(String[] args) {
+        char ch='e';
+        if(ch == 'a'|| ch == 'e' || ch == 'i'|| ch =='o'||ch=='u'){
+            System.out.println("vowel");
+        }else{
+            System.out.println("Consonant");
+        }
+    }
+    
+}
