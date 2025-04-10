3e44d5d7599556a33965860993864cb6daf8af8a
Anurag86135 <anuragmishra11904@gmail.com>
2025-04-10 17:36:33 +0530

    Remainder

---
+++ anurag2.java
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+public class anurag2 {
+    public static void main(String[] args) {
+        int a =29,b=4;
+        System.out.println("Remainder:"+(a%b));
+    }
+    
+}
+=======
+public class anurag2 {
+    public static void main(String[] args) {
+        int a =29,b=4;
+        System.out.println("Remainder:"+(a%b));
+    }
+    
+}
+>>>>>>> 87abcdd2b8ff732b5e5afe42e5520506218e7516
