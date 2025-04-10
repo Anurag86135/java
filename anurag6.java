c678c25d150d28f9b1d2e5b325f5df675be400f5
Anurag86135 <anuragmishra11904@gmail.com>
2025-04-10 16:20:29 +0530

    Positive,,Negative or Zero
    Check if the number is positive , negative or zero

---
+++ anurag6.java
@@ -0,0 +1,14 @@
+public class anurag6 {
+    public static void main(String[] args) {
+        int num = -10;
+        if(num>0){
+            System.out.println("Given number is positive");
+        }
+        else if(num<0){
+            System.out.println("Given number is negative");
+        }else{
+        System.out.println("Given number is zero");
+        }
+    }
+    
+}
