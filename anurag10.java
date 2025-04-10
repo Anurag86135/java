6659b6b96d40dd5bcac587d0f6a62d5b4602424f
Anurag86135 <anuragmishra11904@gmail.com>
2025-04-10 17:06:41 +0530

    Check divisible by 5 and 11 or not.
    Check divisible by 5 and 11 or not.

---
+++ anurag10.java
@@ -0,0 +1,12 @@
+public class anurag10 {
+    public static void main(String[] args) {
+        int num=110;
+        if(num % 5==0 && num % 11==0){
+            System.out.println("Divisisible by 5 and 11");
+        
+        }else{
+            System.out.println("Not divisible by 5 and 11");
+        }
+    }
+    
+}
