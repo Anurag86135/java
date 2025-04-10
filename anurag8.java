310ca56e2965212daf42f2533ffb79ea0f1f1d34
Anurag86135 <anuragmishra11904@gmail.com>
2025-04-10 16:45:43 +0530

    Find the largest of three numbers
    Find the largest of three numbers

---
+++ anurag8.java
@@ -0,0 +1,18 @@
+public class anurag8 {
+    public static void main(String[] args) {
+        int a = 30, b=30, c=30;
+        if(a>b && a>c){
+            System.out.println("a is greatest");
+        }else if(b>c&&b>a){
+            System.out.println("b is greatest");
+
+        }else if(a==b&&b==c){
+            System.out.println("all three numbers are are equals");
+
+        }else{
+            System.out.println("c is greatest");
+        }
+
+    }
+    
+}
